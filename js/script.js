598341a19395f208202a51431ca8337552c75c7e
Simon Shoban <sshoban@tsologic.com>
2018-08-15 12:13:15 -0700

    Created and centered both lists

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-var listOfOthers = ["https://sci-hub.tw", "gen.lib.rus.ec", "strengthlevel.com", "https://standardebooks.org"]
+var listOfProjects = ["http://students.bcitdev.com/A00985653/Tax%20Calculator/taxbuddy.html", "http://evbuddy.ca"];
+var projectList = "projects_list";
+
+var listOfOthers = ["https://sci-hub.tw", "http://gen.lib.rus.ec", "https://strengthlevel.com", "https://standardebooks.org"];
+var otherList = "others_list";
 
 function $(id) {
 	return document.getElementById(id);
 }
 
-function insertListOfOthers() {
-	for (var index = 0; index < listOfOthers.length; index++) {
+function loadLists() {
+	insertListOfOthers(listOfProjects, projectList);
+	insertListOfOthers(listOfOthers, otherList);
+}
+
+function insertListOfOthers(listOfLinks, list) {
+	for (var index = 0; index < listOfLinks.length; index++) {
 		var listElement = document.createElement("li");
 		var linkElement = document.createElement("a");
-		var link		= document.createTextNode(listOfOthers[index]);
+		var link		= document.createTextNode(listOfLinks[index]);
 
-		linkElement.setAttribute("href", listOfOthers[index]);
+		linkElement.setAttribute("href", listOfLinks[index]);
 		linkElement.appendChild(link);
 		listElement.appendChild(linkElement);
-		$("others_list").appendChild(listElement);
+		$(list).appendChild(listElement);
 	}
 }
